10f454463cbc8bbf88a7ff7c05170e08bc8214c3
Winbobob <zhu6@ncsu.edu>
2015-09-05 09:29:36 -0400

    HW1Q4 initial

---
+++ HW1/HW1Q4.R
@@ -0,0 +1,18 @@
+#4. Write code in Matlab, R or Python to perform each of the following tasks,
+#please report your outputs and key codes in the homework file, and also include your
+#code file (end with .m, .r or .py) in the .zip file.
+#(a) Generate a 4*4 matrix A with input from Gaussian Distribution with
+#mean 5 and variance 1.
+#(b) Access rows 2 and 4 only.
+#(c) Calculate sum of the 3nd row, the diagonal and the 4rd column in the
+#matrix.
+#(d) Sum of all elements in the matrix (use a for/while loop).
+#(e) Generate a diagonal matrix B with from [2, 3, 4, 5] (using this vector as the diagonal entries).
+#(f)  From A and B, using one matrix operation to get a new matrix C such
+#that, the first row of C is equal to the first row of A times 2, the second row of C
+#is equal to the second row of A times 3 and so on.
+#(g) From A and B, using one matrix operation to get a new matrix D such
+#that, the first column of D is equal to the first column of A times 2, the second
+#column of D is equal to the second column of A times 3 and so on.
+#(h) X = [1, 2, 3, 4]T, Y = [9, 6, 4, 1]T. Computing the covariance matrix of
+#X and Y in one function, and calculating the result by basic operations (without using that function).
